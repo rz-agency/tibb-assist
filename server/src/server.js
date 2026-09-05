7928c743616fbf1e221d5f7c13da985d5415ecf8
rz-agency <r.z.marketingagency@gmail.com>
2026-09-05 23:35:54 +0500

    Configure backend for Vercel

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,3 @@
 const app = require('./app')
 
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+module.exports = app
